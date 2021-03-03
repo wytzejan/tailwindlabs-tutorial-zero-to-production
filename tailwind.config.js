61b8a72822943eca4f0c9f218727f61a8437f7fc
Wytze Jan Riedstra <wjriedstra@gmail.com>
2021-03-03 20:49:51 +0100

    finish 08: Optimizing for Production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    // enabled: false,
+    content: ["./src/**/*.jsx", "./index.html"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
